c8a9d384c9177d925a287fdeae5bb7582b014dac
AlaaAlTawil <Alaa.m.Tawil@outlook.com>
2022-05-22 14:53:30 +0300

    importing the mp3 files and trying them

---
+++ game.js
@@ -0,0 +1,56 @@
+var blue_sound = new Audio("sounds/sounds/blue.mp3");
+var wrong_sound = new Audio("sounds/sounds/wrong.mp3");
+var green_sound = new Audio("sounds/sounds/green.mp3");
+var red_sound = new Audio("sounds/sounds/red.mp3");
+var yellow_sound = new Audio("sounds/sounds/yellow.mp3");
+var green = document.getElementById("g1");
+var red = document.getElementById("r1");
+var yellow = document.getElementById("y1");
+var blue = document.getElementById("b1");
+
+function start() {
+    green.addEventListener("click", greenFunction);
+    red.addEventListener("click", redFunction);
+    yellow.addEventListener("click", yellowFunction);
+    blue.addEventListener("click", blueFunction);
+    
+}
+
+
+
+function highlight(button) {
+    var orig = button.style.border;
+    button.style.border = ' white';
+    setTimeout(function () {
+        button.style.border = orig;
+    }, 70);
+
+}
+
+
+function greenFunction() {
+    highlight(green);
+    green_sound.play();
+}
+
+function redFunction() {
+    highlight(red);
+    red_sound.play();
+    
+}
+function wrongFunction(){
+    wrong_sound.play();
+}
+
+function yellowFunction() {
+    highlight(yellow);
+    yellow_sound.play();
+}
+
+function blueFunction() {
+    highlight(blue);
+    blue_sound.play();
+}
+
+
+key.addEventListener("keydown",start);
